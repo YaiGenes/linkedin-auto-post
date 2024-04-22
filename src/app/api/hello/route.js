5cce4527d4f5de1f4e7151eb03515a525bb36384
yaiser.avila <yaiser.avila@triggle.app>
2024-04-22 18:14:15 +0200

    Yaiser: fix posts

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { conn } from "../../../libs/mysql";
+import { posts } from "@/db/schema/posts";
 
 export async function GET() {
   const result = await conn.select().from(posts).all();
